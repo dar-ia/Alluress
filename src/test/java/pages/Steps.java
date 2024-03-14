ae8695b4255a5d6e2042b626aabef2a0fe3799b9
dar-ia <iriryt@gmail.com>
2024-03-14 20:44:09 +0400

    Screenshots added

--- src/test/java/pages/Steps.java
+++ src/test/java/pages/Steps.java
@@ -2,7 +2,11 @@ package pages;
 
 import com.codeborne.selenide.*;
 
+import io.qameta.allure.Attachment;
 import io.qameta.allure.Step;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
 import static com.codeborne.selenide.Selenide.*;
 
 public class Steps {
@@ -10,12 +14,14 @@ public class Steps {
     @Step("Opening page")
     public Steps openPage() {
         open("");
+        attachScreenshot();
         return this;
     }
 
     @Step("Opening search field")
     public Steps openSearch() {
         $(".search-input").click();
+        attachScreenshot();
         return this;
     }
 
@@ -24,22 +30,32 @@ public class Steps {
         $("input#query-builder-test")
                 .setValue(repoName)
                 .pressEnter();
+
         return this;
     }
 
     @Step("Opening search result")
     public Steps openingResult() {
+        sleep(10000);
+        attachScreenshot();
         $$("[data-testid='results-list']")
                 .first()
                 .$("a")
                 .click();
+
         return this;
     }
 
     @Step("Asserting that Issues tab is visible")
     public Steps assertThatTabIsVisible() {
+        attachScreenshot();
         $("#issues-tab").shouldBe(Condition.visible);
         return this;
+    }
+
+    @Attachment(value = "Screen", type = "image/png", fileExtension = "png")
+    public byte[] attachScreenshot() {
+        return ((TakesScreenshot)WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
     }
 
 }
